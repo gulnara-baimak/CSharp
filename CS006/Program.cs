1b61a6214c5350a09071b0cd4a7ba3b771ce619b
Gulnara <gulnara@mail.rum>
2022-04-16 01:33:19 +0600

    CS006

---
+++ CS006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ввeдите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+       Console.WriteLine("Hello, Маша");
+}
+else
+{
+    
+        Console.Write("Привет, незнакомка!");
+        
+        Console.WriteLine(username);
+}
