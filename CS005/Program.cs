686e37afe47cfc37f427eaa21e9d5bf3450505d0
Gulnara <gulnara@mail.rum>
2022-04-16 00:59:12 +0600

    CS005

---
+++ CS005/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1, 10); // 1, 2, 3, 4.... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+int result = numberA + numberB + numberC;
+Console.WriteLine(result);
+
